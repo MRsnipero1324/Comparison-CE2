4f73e3b42090190f24a6b21a28d76694c776337a
aaron <aaroncabrales35@gmail.com>
2025-02-09 13:44:11 -0500

    .

--- benchmark.sh
+++ benchmark.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 docker build -t matrix_python -f DockerFiles/Dockerfile.Python . 
-docker run --rm -v $(pwd):/output matrix_python
+docker run --rm -v $(pwd)/output:/output matrix_python
 
 docker build -t matrix_cpp -f DockerFiles/Dockerfile.cpp .
-docker run --rm -v $(pwd):/app/output matrix_cpp
+docker run --rm -v $(pwd)/output:/app/output matrix_cpp
 
 docker build -t matrix_js -f DockerFiles/Dockerfile.javascript .
-docker run --rm -v $(pwd):/output matrix_js
+docker run --rm -v $(pwd)/output:/output matrix_js
 
 docker build -t matrix_go -f DockerFiles/Dockerfile.go .
-docker run --rm -v $(pwd):/output matrix_go
+docker run --rm -v $(pwd)/output:/output matrix_go
 
 docker build -t matrix_rust -f DockerFiles/Dockerfile.rust .
-docker run --rm -v $(pwd):/app/output matrix_rust
+docker run --rm -v $(pwd)/output:/app/output matrix_rust
