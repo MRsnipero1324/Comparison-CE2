94d93e49baa73b037dc820da81d8980bbde87439
aaron <aaroncabrales35@gmail.com>
2025-02-13 22:36:40 -0500

    ---

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Time comparison for the execution of the 5 solutions in the different programmin
 
 apk add nano git
 git clone https://github.com/MRsnipero1324/Comparison-CE2.git
-
+cd Comparison-CE2
 docker build -t benchmark_runner .
 docker run --privileged --rm -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/output:/output benchmark_runner
 
--- run_containers.sh
+++ run_containers.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-# Verificar si Docker está corriendo
-docker info > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-  echo "Docker no está funcionando dentro del contenedor."
-  exit 1
-fi
-
 echo "Construyendo y ejecutando contenedores..."
 
 docker build -t matrix_python -f DockerFiles/Dockerfile.Python . 
 docker run --rm -v $(pwd)/output:/output matrix_python
+ls -l /output  # Verificar los archivos generados
 
 docker build -t matrix_cpp -f DockerFiles/Dockerfile.cpp .
 docker run --rm -v $(pwd)/output:/app/output matrix_cpp
+ls -l /output  # Verificar los archivos generados
 
 echo "Ejecución completada."
 ls -l /output  # Verificar los archivos generados
